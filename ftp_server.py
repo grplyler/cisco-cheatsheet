3c0bc7b3d4b738fece69e3fdc0c4c2c1b9ca46d8
grplyler <g.r.plyler@gmail.com>
2020-01-25 21:51:22 -0500

    Update ftp_server.py

--- ftp_server.py
+++ ftp_server.py
@@ -1,1 +1,42 @@
+#!/usr/bin/env python3
 
+import os
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+def main():
+    # Instantiate a dummy authorizer for managing 'virtual' users
+    authorizer = DummyAuthorizer()
+
+    # anonymous use (WARNING, this will give an anonymous user write access for convienience.)
+    # Use at your own risk.
+    authorizer.add_anonymous(os.getcwd(),  perm='elradfmwMT')
+
+    # Instantiate FTP handler class
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    # Define a customized banner (string returned when client connects)
+    handler.banner = "pyftpdlib based ftpd ready."
+
+    # Specify a masquerade address and the range of ports to use for
+    # passive connections.  Decomment in case you're behind a NAT.
+    #handler.masquerade_address = '151.25.42.11'
+    #handler.passive_ports = range(60000, 65535)
+
+    # Instantiate FTP server class and listen on 0.0.0.0:2121
+    address = ('', 21)
+    server = FTPServer(address, handler)
+
+    # set a limit for connections
+    server.max_cons = 256
+    server.max_cons_per_ip = 5
+
+    # start ftp server
+    print("Serving {} with anonymous read/write access".format(os.getcwd()))
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
